e1171e93483f06f70c227a95faac8ff8c8d1964c
dpeccia <diegoignaciopeccia@gmail.com>
2020-07-13 12:26:07 -0300

    librerias.bat y resultado ejemplo

---
+++ librerias.bat
@@ -0,0 +1,11 @@
+pip install nltk
+pip install -U spacy
+python -m spacy download es_core_news_sm
+pip install beautifulsoup4
+pip install google
+pip install lxml
+pip install colorlog
+pip install unicodedata2
+pip install python-docx
+pip install gensim
+pip install PyYAML
